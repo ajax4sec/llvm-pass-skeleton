5d8374c3ae855520bd4a91a003286f6d0edb2d78
ajax4sec <ajiahit@gmail.com>
2019-04-30 15:57:30 -0400

    llvm pass for running

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -9,48 +9,89 @@
 #include "llvm/IR/Module.h"
 using namespace llvm;
 
-namespace {
-  struct SkeletonPass : public FunctionPass {
-    static char ID;
-    SkeletonPass() : FunctionPass(ID) {}
+namespace
+{
+struct SkeletonPass : public ModulePass
+{
+  static char ID;
+  SkeletonPass() : ModulePass(ID) {}
 
-    virtual bool runOnFunction(Function &F) {
-      // Get the function to call from our runtime library.
-      LLVMContext &Ctx = F.getContext();
-      std::vector<Type*> paramTypes = {Type::getInt32Ty(Ctx)};
-      Type *retType = Type::getVoidTy(Ctx);
-      FunctionType *logFuncType = FunctionType::get(retType, paramTypes, false);
-      Constant *logFunc = F.getParent()->getOrInsertFunction("logop", logFuncType);
+  bool runOnModule(Module &M) override;
+};
+} // namespace
 
-      for (auto &B : F) {
-        for (auto &I : B) {
-          if (auto *op = dyn_cast<BinaryOperator>(&I)) {
-            // Insert *after* `op`.
-            IRBuilder<> builder(op);
-            builder.SetInsertPoint(&B, ++builder.GetInsertPoint());
+bool SkeletonPass::runOnModule(Module &M)
+{
+  LLVMContext &Ctx = M.getContext();
+  std::vector<Type *> paramTypes = {Type::getInt32Ty(Ctx)};
+  Type *retType = Type::getVoidTy(Ctx);
+  FunctionType *logFuncType = FunctionType::get(retType, paramTypes, false);
 
-            // Insert a call to our function.
-            Value* args[] = {op};
-            builder.CreateCall(logFunc, args);
+  std::string module_name = M.getName();
+  char *oldname, *p, *tempp;
+  oldname = new char[module_name.size() + 1];
+  strcpy(oldname, module_name.c_str());
+  p = strtok(oldname, "/");
+  while (p != NULL)
+  {
+    tempp = p;
+    p = strtok(NULL, "/");
+  }
+  module_name = tempp;
+  delete[] oldname;
+  errs() << "Module name: " << module_name << "\n";
 
-            return true;
-          }
+  Constant *logFunc = M.getOrInsertFunction("logop", logFuncType);
+
+  for (auto &F : M)
+  {
+    std::string func_name = F.getName();
+    errs() << "Func name: " << func_name << "\n";
+    for (auto &BB : F)
+    {
+      for (auto &I : BB)
+      {
+        if (auto *op = dyn_cast<BinaryOperator>(&I))
+        {
+          // Insert *after* `op`.
+          IRBuilder<> builder(op);
+          builder.SetInsertPoint(&BB, ++builder.GetInsertPoint());
+
+          // Insert a call to our function.
+          Value *args[] = {op};
+          builder.CreateCall(logFunc, args);
         }
       }
-
-      return false;
     }
-  };
+  }
+
+  return true;
 }
 
 char SkeletonPass::ID = 0;
 
+static RegisterPass<SkeletonPass> X("test", "Test Pass");
+static void registerPass(const PassManagerBuilder &,
+                         legacy::PassManagerBase &PM)
+{
+  PM.add(new SkeletonPass());
+}
+
+static RegisterStandardPasses RegisterAFLPass(
+    PassManagerBuilder::EP_OptimizerLast, registerPass);
+
+static RegisterStandardPasses RegisterAFLPass0(
+    PassManagerBuilder::EP_EnabledOnOptLevel0, registerPass);
+
+/*
 // Automatically enable the pass.
 // http://adriansampson.net/blog/clangpass.html
 static void registerSkeletonPass(const PassManagerBuilder &,
-                         legacy::PassManagerBase &PM) {
+                                 legacy::PassManagerBase &PM)
+{
   PM.add(new SkeletonPass());
 }
 static RegisterStandardPasses
-  RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
-                 registerSkeletonPass);
+    RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
+                   registerSkeletonPass);
+*/
